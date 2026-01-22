d02ba71263abcca2255e2a5dfc2a5b99c2a9ca9f
Collins Biwott <collinsbiwott@Collinss-MacBook-Pro.local>
2026-01-22 15:17:36 +0300

    trails

---
+++ src/textSummarizer/utils/common.py
@@ -0,0 +1,62 @@
+import os
+from box.exceptions import BoxValueError
+import yaml
+from textSummarizer.logging import logger
+from ensure import ensure_annotations
+from box import ConfigBox
+from pathlib import Path
+from typing import Any
+
+@ensure_annotations
+def read_yaml(path_to_yaml: Path) -> ConfigBox:
+    """Reads a YAML file and returns its contents as a ConfigBox object.
+
+    Args:
+        path_to_yaml (str): The path to the YAML file. 
+    Raises:
+        ValueError: If the YAML file is empty or cannot be parsed.
+        e: empty file
+    Returns:
+        ConfigBox: The contents of the YAML file as a ConfigBox object.
+    """
+    try:
+        with open(path_to_yaml) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logger.info(f"YAML file: {path_to_yaml} loaded successfully")
+            return ConfigBox(content)
+    except BoxValueError:
+        raise ValueError("The YAML file is empty or cannot be parsed.")
+    except Exception as e:
+        logger.error(f"Error occurred while reading YAML file: {e}")
+        raise e
+
+
+@ensure_annotations
+def create_directories(path_to_directories: list[Path], verbose=True) -> None:
+    """Creates directories if they do not exist.
+
+    Args:
+        path_to_directories (list[Path]): List of directory paths to create.
+        verbose (bool, optional): If True, logs the creation of directories. Defaults to True.
+    """
+    for path in path_to_directories:
+        os.makedirs(path, exist_ok=True)
+        if verbose:
+            logger.info(f"Directory created at: {path}")
+
+@ensure_annotations
+def getsize(path: Path) -> str:
+    """Gets the size of a file in a human-readable format.
+
+    Args:
+        path (Path): The path to the file.
+
+    Returns:
+        str: The size of the file in a human-readable format.
+    """
+    size_in_bytes = os.path.getsize(path)
+    for unit in ['B', 'KB', 'MB', 'GB', 'TB']:
+        if size_in_bytes < 1024:
+            return f"{size_in_bytes:.2f} {unit}"
+        size_in_bytes /= 1024
+    return f"{size_in_bytes:.2f} PB"
